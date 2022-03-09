d8403b27656b224fb8af9fd920aa4fc1b3b4649e
Amol shelke <shelkeamol897@gmail.com>
2022-03-09 20:02:52 +0530

    Delete before folder and created another folder

--- Anime profiles/js/script.js
+++ Anime profiles/js/script.js
@@ -5,7 +5,7 @@ const animeName = document.querySelector(".hero-name");
 
 const api_url = `https://api.catboys.com/img`;
 
-getBTN.addEventListener("click", async () => {
+getBTN.addEventListener("click", async function () {
   const response = await fetch(api_url);
   const data = await response.json();
   anime.src = data.url;
