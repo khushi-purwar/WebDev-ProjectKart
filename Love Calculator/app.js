dba2ad0978ee320746c9cd541b4346afdef238d8
iamrahulmahato <rahul25mahato@gmail.com>
2022-03-26 11:26:14 -0700

    :hammer: crushname varible added

--- Love Calculator/app.js
+++ Love Calculator/app.js
@@ -6,5 +6,6 @@ window.onload = function() {
 
 function calculateLove() {
     let yourName = document.getElementById("your-name").value;
+    let crushName = document.getElementById("crush-name").value;
 
 }
