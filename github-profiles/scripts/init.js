6ddf1c3507f6b62fbb489836f7b70e056460a123
kalash <kalashvasaniya@gmail.com>
2022-05-03 11:11:56 +0530

    adding github profile

---
+++ github-profiles/scripts/init.js
@@ -0,0 +1,5 @@
+const submitButton = document.querySelector("#submit-button");
+const usernameField = document.querySelector("#username-field");
+const darkToggle = document.querySelector("#dark-toggle");
+const themeName = document.querySelector('#theme-name');
+let root = document.documentElement;
