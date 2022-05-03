246c7bf04f4514af3cd6fbc0954cc921be087017
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-02 17:45:01 -0700

    :memo: function added

--- Flip Card Game/js/script.js
+++ Flip Card Game/js/script.js
@@ -15,3 +15,28 @@ const clearBtn = document.getElementById("clear-btn");
 let currentActiveCard = 0;
 
 const cardsData = getCardsData();
+
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+}
+
+function createCard(data, index) {
+
+    const card = document.createElement("div");
+
+    card.classList.add("card");
+
+    if (index === 0) {
+        card.classList.add("active");
+    }
+
+    card.innerHTML = `
+          <div class="inner-card">
+              <div class="card-front">
+                  <p>${data.question}</p>
+              </div>
+              <div class="card-back">
+                  <p>${data.answer}</p>
+              </div>
+          </div>
+      `;
