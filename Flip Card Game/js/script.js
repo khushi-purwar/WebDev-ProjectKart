5344495a6ccff5aaa6d10ba2f8e6fa6a571f84c8
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-02 17:43:16 -0700

    :memo: card container added

--- Flip Card Game/js/script.js
+++ Flip Card Game/js/script.js
@@ -2,3 +2,10 @@ const cardContainer = document.getElementById("card-container");
 
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
+
+const addCardContainer = document.getElementById("add-card-container");
+const addCardBtn = document.getElementById("add-card");
+const closeCardBtn = document.getElementById("close-card");
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const addNewCardBtn = document.getElementById("add-card-btn");
