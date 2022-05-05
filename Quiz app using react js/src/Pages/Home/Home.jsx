c1d2f02192a8c9af23b794f48746d5a16fbf9bf6
yash003singhal <yash003singhal@gmail.com>
2022-05-05 17:32:12 +0530

    quiz app using reactjs and trivi api

---
+++ Quiz app using react js/src/Pages/Home/Home.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import "./Home.css";
+import QuizImage from "../../assets/Quiz.png";
+import Categories from "../../data/Categories";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+
+const Home = ({ name, setName, fetchQuestions }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      history.push("/quiz");
+    }
+  };
+
+  return (
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>Quiz Setting</span>
+
+        <div className="settings__select">
+          {error && <ErrorMessage>Please fill all the feilds</ErrorMessage>}
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter your name"
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            label="Select Difficulty"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+            onChange={(e) => setDifficulty(e.target.value)}
+            value={setDifficulty}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSubmit}
+          >
+            Start Quiz
+          </Button>
+        </div>
+      </div>
+      <img className="banner" src={QuizImage} alt="Quiz Image" />
+    </div>
+  );
+};
+
+export default Home;
---
+++ Quiz app using react js/src/Pages/Quiz/Quiz.jsx
@@ -0,0 +1,59 @@
+import { CircularProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import Question from "../../components/Question/Question";
+import "./Quiz.css";
+
+const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
+        ])
+    );
+  }, [currQues, questions]);
+
+  console.log(questions);
+
+  const handleShuffle = (options) => {
+    return options.sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div className="quiz">
+      <span className="subtitle">Welcome, {name}</span>
+
+      {questions ? (
+        <>
+          <div className="quizInfo">
+            <span>{questions[currQues].category}</span>
+            <span>Score : {score}</span>
+          </div>
+          <Question
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </>
+      ) : (
+        <CircularProgress
+          style={{ margin: 100 }}
+          color="inherit"
+          size={150}
+          thickness={1}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
