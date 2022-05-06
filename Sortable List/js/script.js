fb1150668c6ad59027dd070e1d55b4dec2d58190
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-06 16:09:28 -0700

    :memo: function created to store list in DOM

--- Sortable List/js/script.js
+++ Sortable List/js/script.js
@@ -16,3 +16,34 @@ const richestPeople = [
 
 // Store listitems
 const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+    [...richestPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((person, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+        });
+
+    addEventListeners();
+}
