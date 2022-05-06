0a4e71a23c15899668c0db97d0e18e6342850243
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-06 16:06:53 -0700

    :memo: richest people list added

--- Sortable List/js/script.js
+++ Sortable List/js/script.js
@@ -1,2 +1,15 @@
 const draggable_list = document.getElementById('draggable-list');
 const check = document.getElementById('check');
+
+const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'
+];
