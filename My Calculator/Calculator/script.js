0e164f7940fec162f09ab6f8437d5f6c2807a28d
Aisha <100334178+i-am-aisha@users.noreply.github.com>
2022-05-09 16:24:38 +0530

    Add files via upload

---
+++ My Calculator/Calculator/script.js
@@ -0,0 +1,20 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
