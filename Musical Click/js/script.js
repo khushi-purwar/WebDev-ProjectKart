026f16199004e46a0178744400cb889602525d15
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-09 07:25:15 -0700

    :memo: music var created

---
+++ Musical Click/js/script.js
@@ -0,0 +1,16 @@
+const WORK_IT = new Audio("./audio/WORK_IT.wav");
+const MAKE_IT = new Audio("./audio/MAKE_IT.wav");
+const DO_IT = new Audio("./audio/DO_IT.wav");
+const MAKES_US = new Audio("./audio/MAKES_US.wav");
+const HARDER = new Audio("./audio/HARDER.wav");
+const BETTER = new Audio("./audio/BETTER.wav");
+const FASTER = new Audio("./audio/FASTER.wav");
+const STRONGER = new Audio("./audio/STRONGER.wav");
+const MORE_THAN = new Audio("./audio/MORE_THAN.wav");
+const HOUR = new Audio("./audio/HOUR.wav");
+const OUR = new Audio("./audio/OUR.wav");
+const NEVER = new Audio("./audio/NEVER.wav");
+const EVER = new Audio("./audio/EVER.wav");
+const AFTER = new Audio("./audio/AFTER.wav");
+const WORK_IS = new Audio("./audio/WORK_IS.wav");
+const OVER = new Audio("./audio/OVER.wav");
