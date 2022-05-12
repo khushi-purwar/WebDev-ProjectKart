bfc0a3799069d5aebbd933fe3a87736ec00897c0
mayank-del <mkjam.007@gmail.com>
2022-05-12 13:01:25 +0530

    todoApp-rebuild

--- TO-DO App/script/script.js
+++ TO-DO App/script/script.js
@@ -1,160 +1,84 @@
-var taskInput=document.getElementById("new-task");
-var addButton=document.getElementsByTagName("button")[0];
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");
-var completedTasksHolder=document.getElementById("completed-tasks");
-
-
-var createNewTaskElement=function(taskString){
-
-	var listItem=document.createElement("li");
-
-	
-	var checkBox=document.createElement("input");//checkbx
-	
-	var label=document.createElement("label");//label
-	
-	var editInput=document.createElement("input");//text
-	
-	var editButton=document.createElement("button");//edit button
-
-
-	var deleteButton=document.createElement("button");//delete button
-
-	label.innerText=taskString;
-
-	checkBox.type="checkbox";
-	editInput.type="text";
-
-	editButton.innerText="Edit";//innerText encodes special characters, HTML does not.
-	editButton.className="edit";
-	deleteButton.innerText="Delete";
-	deleteButton.className="delete";
 
 
 
+let theInput = document.querySelector(".add-task input");
+let theAddButton = document.querySelector(".add-task .plus");
+let tasksContainer = document.querySelector(".tasks-content");
+let noTasksMsg = document.querySelector(".no-tasks-message");
+let tasksCount = document.querySelector(".tasks-count span ");
+let tasksCompleted = document.querySelector(".tasks-completed span");
+let count=0;
+let finished=0;
+window.onload = function () {
+  theInput.focus();
+};
+theAddButton.onclick = function () {
+  if (theInput.value === "") {
+    alert("You should add a task");
+  } else {
+    noTasksMsg.remove();  
+    let mainSpan = document.createElement("span");
+    count++;  
+    let textSpan=document.createElement("span")
+    let deleteElement = document.createElement("span");
+    let editElement = document.createElement("span");
 
-	listItem.appendChild(checkBox);
-	listItem.appendChild(label);
-	listItem.appendChild(editInput);
-	listItem.appendChild(editButton);
-	listItem.appendChild(deleteButton);
-	return listItem;
-}
+   
+    let text = document.createTextNode(theInput.value);
+    
+  
+    let texting=document.createTextNode(theInput.value);
+    let deleteText = document.createTextNode("Delete");
+    let editText = document.createTextNode("Edit");
+    console.log(texting);
+   
+    mainSpan.className = "task-box";
 
+    textSpan.appendChild(texting);
+    deleteElement.appendChild(deleteText);
+    editElement.appendChild(editText);
+    textSpan.className="texts"
+    deleteElement.className = "delete";
+    editElement.className="edit"
+    mainSpan.appendChild(textSpan);
+    mainSpan.appendChild(deleteElement);
+    mainSpan.appendChild(editElement);
+    tasksContainer.appendChild(mainSpan);
+    theInput.value = "";
+    theInput.focus();
+    
+    tasksCount.textContent=count;
+    tasksCompleted.textContent=finished;
+  }
+};
+document.addEventListener("click", function (e) {
 
-
-var addTask=function(){
-	console.log("Add Task...");
-	//Create a new list item with the text from the #new-task:
-	var listItem=createNewTaskElement(taskInput.value);
-
-	//Append listItem to incompleteTaskHolder
-	incompleteTaskHolder.appendChild(listItem);
-	bindTaskEvents(listItem, taskCompleted);
-
-	taskInput.value="";
-
-}
-
-
-var editTask=function(){
-console.log("Edit Task...");
-console.log("Change 'edit' to 'save'");
-
-
-var listItem=this.parentNode;
-
-var editInput=listItem.querySelector('input[type=text]');
-var label=listItem.querySelector("label");
-var containsClass=listItem.classList.contains("editMode");
-		
-		if(containsClass){
-
-		
-			label.innerText=editInput.value;
-		}else{
-			editInput.value=label.innerText;
-		}
-
-
-		listItem.classList.toggle("editMode");
-}
-
-
-
-var deleteTask=function(){
-		console.log("Delete Task...");
-
-		var listItem=this.parentNode;
-		var ul=listItem.parentNode;
-		//Remove the parent list item from the ul.
-		ul.removeChild(listItem);
-
-}
-
-
-var taskCompleted=function(){
-		console.log("Complete Task...");
-	
-	
-	var listItem=this.parentNode;
-	completedTasksHolder.appendChild(listItem);
-				bindTaskEvents(listItem, taskIncomplete);
-
-}
-
-
-var taskIncomplete=function(){
-		console.log("Incomplete Task...");
-		var listItem=this.parentNode;
-	incompleteTaskHolder.appendChild(listItem);
-			bindTaskEvents(listItem,taskCompleted);
-}
+  if (e.target.className == "delete") {
+    count--;
+    if(!e.target.classList.contains('finished')){
+      finished--;
+    }
+    e.target.parentNode.remove();
+  }
+  if (e.target.classList.contains("task-box")) {
+    e.target.classList.toggle("finished");
+    if(e.target.classList.contains('finished')){
+    	finished++;
+    }else{
+    	finished--;
+    }
+  }
+  else if(e.target.className=="edit"){
+    
+    console.log("edit button pressed")
+    theInput.value=e.target.previousElementSibling.previousSibling.innerHTML
+    e.target.parentNode.remove();
 
 
-
-var ajaxRequest=function(){
-	console.log("AJAX Request");
-}
-
-
-
-
-
-addButton.onclick=addTask;
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
-
-
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-	console.log("bind list item events");
-
-	var checkBox=taskListItem.querySelector("input[type=checkbox]");
-	var editButton=taskListItem.querySelector("button.edit");
-	var deleteButton=taskListItem.querySelector("button.delete");
-
-
-			
-			editButton.onclick=editTask;
-			deleteButton.onclick=deleteTask;	
-			checkBox.onchange=checkBoxEventHandler;
-}
-
-
-	for (var i=0; i<incompleteTaskHolder.children.length;i++){
-
-		bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
-	}
-
-
-
-
-
-	for (var i=0; i<completedTasksHolder.children.length;i++){
-	
-		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
-	}
-
+  }
+  tasksCount.textContent=count;
+    tasksCompleted.textContent=finished;
+});
 
 
 
