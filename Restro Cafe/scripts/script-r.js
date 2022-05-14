c3ad5b6dd29761eb5fadaa1c851222490a7c49ee
Dipayan Bose <76175101+bosedipayan@users.noreply.github.com>
2022-05-14 10:36:47 +0530

    Added Cafe Site

---
+++ Restro Cafe/scripts/script-r.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-top').classList.add('active');
+    }
+    else{
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+
+function fadeout() {
+    setInterval(loader,3000);
+}
+
+
+window.onload = fadeout();
+
+
